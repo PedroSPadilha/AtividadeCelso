92dd046e04c99a2abf421291ecfd637b6220f82e
pedro.padilha <pedro.padilha@legalcontrol.com.br>
2025-03-22 14:18:38 -0300

    Up

--- Atividade1/Atividade1/obj/Debug/net8.0/Atividade1.AssemblyInfo.cs
+++ Atividade1/Atividade1/obj/Debug/net8.0/Atividade1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad52e6ff26f2bb6a292a3a0380e72f3813f003a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
